6acf73d9121d25b75ea1d665d19eac70fac52dac
mwojterski <mwojterski@gmail.com>
2014-10-05 14:23:42 +0200

    Template for text route

--- src/main/scala/pl/mwojterski/ShowUp.scala
+++ src/main/scala/pl/mwojterski/ShowUp.scala
@@ -1,18 +1,38 @@
 package pl.mwojterski
 
-import akka.actor._
+import akka.actor.ActorSystem
 import spray.routing.SimpleRoutingApp
 
 object ShowUp extends App with SimpleRoutingApp {
-  implicit val system = ActorSystem("simple-system")
+  private implicit val system = ActorSystem("simple-system")
 
-  startServer(interface = "localhost", port = 8080) {
-    path("hello") {
-      parameters('id) { id =>
+  private val route = path("route") {
+    parameters('id) { id =>
+      complete {
+        s"Passed id='$id'"
+      }
+    }
+  }
+
+  private val text = path("text") {
+    // implicits for response marshalling
+    import spray.httpx.SprayJsonSupport.sprayJsonMarshaller
+    import spray.json.DefaultJsonProtocol.{mapFormat, StringJsonFormat}
+
+    implicit val printer = spray.json.CompactPrinter // by default marshaller uses PrettyPrinter
+
+    get {
+      parameterMap { files =>
         complete {
-          s"Passed id='$id'ss"
+          files.transform {
+            (file, line) => s"Text for line $line of file '$file'"
+          }
         }
       }
     }
+  }
+
+  startServer(interface = "localhost", port = 8080) {
+     route ~ text
   }
 }
